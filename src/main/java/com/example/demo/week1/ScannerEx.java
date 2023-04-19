140e02cf4da87501734bd68207c27aa98e193cf2
tlsgudwns <hyeongjunsin@gmail.com>
2023-04-19 15:29:09 +0900

    feat: 마지막 크롤 시간 작업1 - CrawlDate 도메인 추가

---
+++ src/main/java/com/example/demo/week1/ScannerEx.java
@@ -0,0 +1,13 @@
+package com.example.demo.week1;
+
+import java.util.Scanner;
+
+public class ScannerEx {
+    public void plusTwoNumbers(int val1, int val2) {
+
+    }
+    public void readTwoNumberAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
---
+++ src/main/java/com/example/demo/week1/ScannerExTest.java
@@ -0,0 +1,8 @@
+package com.example.demo.week1;
+
+public class ScannerExTest {
+    public static void main(String[] args) {
+        ScannerEx se = new ScannerEx();
+        se.readTwoNumberAndPlus();
+    }
+}
