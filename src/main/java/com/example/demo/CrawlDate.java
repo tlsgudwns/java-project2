b335091d1228825deb2d90f6858abb2187d2ec65
tlsgudwns <hyeongjunsin@gmail.com>
2023-04-19 13:23:40 +0900

    feat: 마지막 크롤 시간 작업1 - CrawlDate 도메인 추가

---
+++ src/main/java/com/example/demo/CrawlDate.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import java.time.LocalDateTime;
+
+public class CrawlDate {
+    private LocalDateTime lastCrawlDateTime;
+
+    public CrawlDate(LocalDateTime lastCrawlDateTime) {
+        this.lastCrawlDateTime = lastCrawlDateTime;
+    }
+}
