bdd1a2bd01cbe7fe46dc70656b8cbe3ee198488e
tlsgudwns <hyeongjunsin@gmail.com>
2023-04-19 11:41:42 +0900

    a

---
+++ src/main/java/com/example/demo/ByeClass.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class ByeClass {
+    public void print() {
+        System.out.println("Bye");
+    }
+}
---
+++ src/main/java/com/example/demo/ByeClassTest.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class ByeClassTest {
+
+    public static void main(String[] args) {
+        ByeClass byeClass = new ByeClass();
+        byeClass.print();
+    }
+}
