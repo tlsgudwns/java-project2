9fc8cde6877e001c587609a8cc5b7efabca3e411
tlsgudwns <hyeongjunsin@gmail.com>
2023-04-19 11:21:36 +0900

    a

---
+++ src/main/java/com/example/demo/PrintHello.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class PrintHello {
+    public void print() {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/com/example/demo/PrintHelloTest.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class PrintHelloTest {
+    public static void main(String[] args) {
+        PrintHello printHello = new PrintHello();
+        printHello.print();
+    }
+}
